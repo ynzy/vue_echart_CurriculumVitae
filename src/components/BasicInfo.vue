c80608a40063a16cc8c7772e91d3a2c189d31730
ynzy <a758922096@gmail.com>
2019-04-20 15:02:06 +0800

    工作经验

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -8,9 +8,9 @@
     <p>年&emsp;&emsp;龄：23</p>
     <p>联系电话：18131239692</p>
     <p>电子邮箱：758922096@qq.com</p>
-    <p>博&emsp;&emsp;客：<a href="https://ynzy.github.io">ynzy.github.io</a></p>
-    <p>简&emsp;&emsp;书：<a href="https://www.jianshu.com/u/a119a26f3e98">云翼飞</a></p>
-    <p>GitHub：<a href="https://github.com/ynzy">ynzy</a></p>
+    <p>博&emsp;&emsp;客：<a href="https://ynzy.github.io" target="view_window">ynzy.github.io</a></p>
+    <p>简&emsp;&emsp;书：<a href="https://www.jianshu.com/u/a119a26f3e98" target="view_window">云翼飞</a></p>
+    <p>GitHub：<a href="https://github.com/ynzy" target="view_window">ynzy</a></p>
   </div>
 </template>
 
--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -1,6 +1,20 @@
 <!-- 工作经验 -->
 <template>
-  <div :class="WorkExperience">工作经验</div>
+  <div :class="WorkExperience">
+    <h3 class="text-center text-top">工作经验</h3>
+    <p>
+      <a href="javascript:void(0)">保定点动科技有限公司</a>
+      <span class="text-small">| 2018/06 - 至今</span>
+    </p>
+    <p class="text-small">工作内容：apicloud业务app、Vue </p>
+    <p class="text-small">项目成就：</p>
+    <p class="text-small">&emsp;1. 诚通项目外挂系统，完成PC端后台，手机app订单录入查询等功能，已经交付</p>
+    <p class="text-small">&emsp;2. 搭建easy-mock数据模拟平台，用于前端数据模拟，已经完成内部部署</p>
+    <p class="text-small">&emsp;3. 全民小说阅读项目，完成小说阅读H5开发，<a href="http://shunpingreadbook.diandongmark.com/index.html
+" target="view_window">已经部署上线</a>
+</p>
+
+  </div>
 </template>
 
 <script>
@@ -26,4 +40,24 @@ export default {
   border-image: url("~@/./assets/img/border_image.png") 30 30 stretch;
   background: #18202d;
 }
+a {
+  color: deepskyblue;
+}
+a:hover {
+  color: rgb(118, 190, 248);
+}
+.part-five-responsive {
+  width: 100%;
+  border: 40px solid transparent;
+  border-image: url("~@/./assets/img/border_image.png") 30 30 stretch;
+  background: #18202d;
+}
+.text-center {
+  text-align: center;
+}
+.text-small {
+  font-size: 0.9em;
+  color: rgb(253, 239, 239);
+}
+
 </style>
