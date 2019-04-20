8f1365e1862e48b1b8fff1028de132ed0b75b693
ynzy <a758922096@gmail.com>
2019-04-20 15:15:04 +0800

    编程技能

--- src/components/ProgrammingSkills.vue
+++ src/components/ProgrammingSkills.vue
@@ -1,6 +1,11 @@
 <!-- 编程技能 -->
 <template>
-  <div :class="ProgrammingSkills">编程技能</div>
+  <div :class="ProgrammingSkills">
+     <h3 class="text-center">编程技能</h3>
+    <p class="font-small"><span class="font-bold">前端：</span>HTML/HTML5、CSS/CSS3、Less/Sass、JS/ES6、jQuery、Vue技术栈、apicloud</p>
+    <p class="font-small"><span class="font-bold">开发环境：</span>Node、VsCode、apicloud、Hubilder</p>
+    <p class="font-small"><span class="font-bold">其他：</span>MongoDB、pm2</p>
+  </div>
 </template>
 
 <script>
@@ -25,5 +30,21 @@ export default {
   border: 15px solid transparent;
   border-image: url("~@/./assets/img/border_image.png") 30 30 stretch;
   background: #18202d;
+}
+.part-six-responsive {
+  width: 100%;
+  border: 40px solid transparent;
+  border-image: url("~@/./assets/img/border_image.png") 30 30 stretch;
+  background: #18202d;
+}
+.text-center {
+  text-align: center;
+}
+.font-small {
+  font-size: .9em;
+}
+.font-bold {
+  font-weight: bold;
+  color: deepskyblue;
 }
 </style>
